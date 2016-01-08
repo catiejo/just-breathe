5f0b6927f8bff092c6f8d310d4032c68f4f9c8e4
Catie Jo <catiejo.pidel@gmail.com>
2016-01-07 18:08:02 -0800

    initial commit

---
+++ Assets/Wave.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var anim : Animation;
+
+function Start () {
+	anim = GetComponent.<Animation>();
+}
+
+function Update () {
+
+}
+
+public function Breathe() {
+	anim.Play("breathe_better");
+	anim.Play("breathe_better");
+	anim.Play("breathe_better");
+}
